21e45d5e06c1582322cd6f3d370aa8724b527a70
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-10-21 17:49:24 +0300

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "material-icons/iconfont/material-icons.css";
 import "./globals.css";
 
 const geistSans = localFont({
---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import style from "./Footer.module.scss";
+
+const navItems = [
+  {
+    icon: "home",
+    link: "/",
+  },
+  {
+    icon: "explore",
+    link: "/explore",
+  },
+  {
+    icon: "place",
+    link: "/place",
+  },
+  {
+    icon: "person_outline",
+    link: "/profile",
+  },
+];
+
+const Footer = () => {
+  const { push } = useRouter();
+  const pathname = usePathname();
+  return (
+    <footer className={style.footer}>
+      <nav>
+        {navItems.map((item) => (
+          <button
+            className={pathname === item.link ? style.active : ""}
+            key={item.icon}
+            onClick={() => push(item.link)}
+          >
+            <span className="material-icons-outlined">{item.icon}</span>
+          </button>
+        ))}
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
